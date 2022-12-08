453d545c617d4b7f3455b3184da5548f294ae6cd
Felix Parker <flixpar@gmail.com>
2022-12-08 05:00:06 -0500

    upgrade capacity plot

--- public/js/capacity_plot.js
+++ public/js/capacity_plot.js
@@ -1,5 +1,8 @@
 export { createCapacityPlot };
 
+
+const traceColors = ["#006C67", "#B9314F", "#454E9E", "#95B46A", "#B6C2D9", "#9370DB", "blue"];
+
 function createCapacityPlot(response) {
 	const capacity = response.capacity;
 	const N = capacity.length;
@@ -15,23 +18,40 @@ function createCapacityPlot(response) {
 			type: "scatter",
 			mode: "lines",
 			name: response.config.node_names[i],
+			line: {
+				color: traceColors[i],
+				width: 3,
+			},
 		};
 		return t;
 	});
 
 	const layout = {
-		title: "Capacity",
 		xaxis: {
 			title: "Date",
-			showgrid: false,
-			zeroline: false,
+			tickformat: "%Y-%m-%d",
 		},
 		yaxis: {
 			title: "Capacity",
-			showline: false,
 			rangemode: "tozero",
 		},
 	};
 
-	Plotly.newPlot("section-results-capacity", traces, layout);
+	const config = {
+		scrollZoom: false,
+		toImageButtonOptions: {
+			format: "svg",
+			filename: "capacity_plot",
+			height: 500,
+			width: 900,
+			scale: 1,
+		},
+		modeBarButtonsToRemove: ["zoom2d", "pan2d", "select2d", "lasso2d", "zoomIn2d", "zoomOut2d", "autoScale2d", "resetScale2d"],
+	};
+
+	let section = document.getElementById("section-results-capacity");
+	let wrapper = document.createElement("div");
+	wrapper.id = "capacity-plot-wrapper";
+	section.appendChild(wrapper);
+	Plotly.newPlot("capacity-plot-wrapper", traces, layout, config);
 }
